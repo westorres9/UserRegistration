c12facacd9079e73fe6cd9bad9351b19de0d6eb7
westorres9 <wester.torres9@gmail.com>
2022-05-25 20:05:10 -0300

    projeto inicial criado

---
+++ backend/src/main/java/com/fullstackdevelopment/userregistration/UserregistrationApplication.java
@@ -0,0 +1,13 @@
+package com.fullstackdevelopment.userregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserregistrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserregistrationApplication.class, args);
+    }
+
+}
---
+++ backend/src/main/java/com/fullstackdevelopment/userregistration/entities/User.java
@@ -0,0 +1,66 @@
+package com.fullstackdevelopment.userregistration.entities;
+
+import java.util.Objects;
+
+public class User {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String address;
+    
+    public User() {
+    }
+
+    public User(Long id, String name, String email, String address) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
