5636016f60d86d9aa1a5a7a31810becc170f7bbb
westorres9 <wester.torres9@gmail.com>
2022-05-25 20:53:40 -0300

    implementada classes UserDTO e UserRepository

---
+++ backend/src/main/java/com/fullstackdevelopment/userregistration/dto/UserDTO.java
@@ -0,0 +1,60 @@
+package com.fullstackdevelopment.userregistration.dto;
+
+import com.fullstackdevelopment.userregistration.entities.User;
+
+public class UserDTO {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String address;
+
+    public UserDTO() {
+    }
+
+    public UserDTO(Long id, String name, String email, String address) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.address = address;
+    }
+
+    public UserDTO(User entity) {
+        this.id = entity.getId();
+        this.name = entity.getName();
+        this.email = entity.getEmail();
+        this.address = entity.getAddress();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
