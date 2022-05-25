86ffdf2400a4d611f895afde68279b8f3606b939
westorres9 <wester.torres9@gmail.com>
2022-05-25 20:10:11 -0300

    implementada entidade User e mapeamento JPA

--- backend/src/main/java/com/fullstackdevelopment/userregistration/entities/User.java
+++ backend/src/main/java/com/fullstackdevelopment/userregistration/entities/User.java
@@ -1,9 +1,15 @@
 package com.fullstackdevelopment.userregistration.entities;
 
+import javax.persistence.*;
+import java.io.Serializable;
 import java.util.Objects;
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
 
-public class User {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
