0ac5f0d459d09cbeed5fc2f4ed897144a9cf9e41
westorres9 <wester.torres9@gmail.com>
2022-05-25 23:31:15 -0300

    implementado metodos findAll findById Insert Update Delete para UserService na camada de serviço

--- backend/src/main/java/com/fullstackdevelopment/userregistration/services/UserService.java
+++ backend/src/main/java/com/fullstackdevelopment/userregistration/services/UserService.java
@@ -3,10 +3,16 @@ package com.fullstackdevelopment.userregistration.services;
 import com.fullstackdevelopment.userregistration.dto.UserDTO;
 import com.fullstackdevelopment.userregistration.entities.User;
 import com.fullstackdevelopment.userregistration.repositories.UserRepository;
+import com.fullstackdevelopment.userregistration.services.exceptions.DatabaseException;
+import com.fullstackdevelopment.userregistration.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class UserService {
@@ -18,5 +24,49 @@ public class UserService {
     public List<UserDTO> findAll() {
         List<User> list = repository.findAll();
         return list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public UserDTO findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        User entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity Not Found"));
+        return new UserDTO(entity);
+    }
+
+    @Transactional
+    public UserDTO insert(UserDTO dto) {
+        User entity = new User();
+        entity.setName(dto.getName());
+        entity.setEmail(dto.getEmail());
+        entity.setAddress(dto.getAddress());
+        entity = repository.save(entity);
+        return new UserDTO(entity);
+    }
+
+    @Transactional
+    public UserDTO update(Long id, UserDTO dto) {
+        try {
+            User entity = repository.getOne(id);
+            entity.setName(dto.getName());
+            entity.setEmail(dto.getEmail());
+            entity.setAddress(dto.getAddress());
+            entity = repository.save(entity);
+            return new UserDTO(entity);
+        }
+        catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Id not found " + id);
+        }
+    }
+
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        }
+        catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException("Id not found " + id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Integrity Violation");
+        }
     }
 }
