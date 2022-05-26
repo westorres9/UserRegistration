70e7cc5ba2c03bbc775ce7d23edfb69b270a6be5
westorres9 <wester.torres9@gmail.com>
2022-05-26 00:45:01 -0300

    implementada classe StandardError para tratamento de exceções na camada de recurso

--- backend/src/main/java/com/fullstackdevelopment/userregistration/dto/UserDTO.java
+++ backend/src/main/java/com/fullstackdevelopment/userregistration/dto/UserDTO.java
@@ -2,7 +2,10 @@ package com.fullstackdevelopment.userregistration.dto;
 
 import com.fullstackdevelopment.userregistration.entities.User;
 
-public class UserDTO {
+import java.io.Serializable;
+
+public class UserDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     private Long id;
     private String name;
